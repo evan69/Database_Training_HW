53f13d4d6c69e3d25027f1deea1b94981d8209ec
evan69 <evan9669@sina.com>
2017-05-06 09:53:26 -0700

    use partition method and AC it

--- exp2/SimJoiner.cpp
+++ exp2/SimJoiner.cpp
@@ -10,8 +10,11 @@ using namespace std;
 
 vector<string> line_vec;
 //vector of lines in input file
+vector<string> line_vec2;
+//vector of lines in input file
 int dis[4096][4096];
 //used in DP calculation
+/*
 vector<vector<int> > gram_inverted_list;
 //inverted list for ED
 vector<vector<int> > gram_inverted_list_all;
@@ -22,8 +25,26 @@ map<string,int> gram_set;
 //set of gram for ED
 map<string,int> word_set;
 //set of word for JAC
+*/
+struct Segment
+{
+	unsigned segNo;
+	map<string,int> invMap;
+	vector<vector<int> > invList;
+};
 
-int qq = 2;
+struct Group
+{
+	Segment segs[512];
+	unsigned len;
+	bool used = false;
+};
+
+Group partition_inverted_list[512];
+Group partition_inverted_list2[512];
+
+unsigned qq = 3;
+unsigned tau;
 
 SimJoiner::SimJoiner() {
 	for (int i = 0; i < DIS_SIZE; i++)
@@ -35,35 +56,10 @@ SimJoiner::SimJoiner() {
 SimJoiner::~SimJoiner() {
 }
 
-int split_gram(const string& str, int q, vector<string>& result, int num)
-{
-	result.clear();
-	int i = 0;
-	int len = str.length();
-	while(i + q - 1 < len)
-	{
-		string qgram_entry = string(q,'*');
-		for(int j = 0;j < q;j++)
-		{
-			qgram_entry[j] = str[j + i];
-		}
-		//qgram_entry[q] = '\0';
-		//cout << "entry:" << qgram_entry << endl;
- 		result.push_back(qgram_entry);
-		i++;
-		if(i > num)
-			break;
-	}
-	sort(result.begin(),result.end());
-    //result.erase(unique(result.begin(),result.end()),result.end());
-	return SUCCESS;
-}
-
-int readin(const char *filename)
+int readin(const char* filename1, const char *filename2)
 // read in data
 {
-	ifstream fin(filename);
-	int id = 0;
+	ifstream fin(filename1);
 	string string_buf;
 	while(!fin.eof())
 	{
@@ -73,10 +69,23 @@ int readin(const char *filename)
 			break;
 		}
 		line_vec.push_back(string_buf);
-		id++;
 	}
 	
 	fin.close();
+
+	ifstream fin2(filename2);
+	while(!fin2.eof())
+	{
+		getline(fin2,string_buf);
+		if(string_buf.size() == 0)
+		{
+			break;
+		}
+		line_vec2.push_back(string_buf);
+	}
+	
+	fin2.close();
+
 	return SUCCESS;
 }
 
@@ -84,12 +93,6 @@ int readin(const char *filename)
 int calED(const string& query, const string& entry, int th)
 //calculate ED between query and entry
 {
-	/*
-	const char* t = query;
-	int lent = strlen(t);
-	const char* s = entry;
-	int lens = strlen(s);
-	*/
 	const string& t = query;
 	int lent = t.length();
 	const string& s = entry;
@@ -137,106 +140,239 @@ int calED(const string& query, const string& entry, int th)
 	return dis[lens][lent];
 }
 
+int split_partition(const string& str, unsigned len, vector<string>& result, unsigned offset)
+{
+	result.clear();
+	unsigned i = 0;
+	unsigned pos = offset;
+	//unsigned len = str.length();
+	unsigned length1 = floor(1.0 * len / (tau + 1));
+	unsigned length2 = ceil(1.0 * len / (tau + 1));
+	//assert(length1 > 0 && length2 > 0);
+	unsigned num = len % (tau + 1);
+	//assert(len == length2 * num + (tau + 1 - num) * length1);
+	while(i < tau + 1)
+	{
+		unsigned real_len = (i < num) ? length2 : length1;
+		string partition_entry = string(real_len,'*');
+		for(unsigned j = 0;j < real_len;j++)
+		{
+			//assert(pos < str.length());
+			partition_entry[j] = str[pos];
+			pos ++;
+		}
+		i++;
+ 		result.push_back(partition_entry);
+	}
+	return SUCCESS;
+}
+
 int SimJoiner::joinJaccard(const char *filename1, const char *filename2, double threshold, vector<JaccardJoinResult> &result) {
     result.clear();
     return SUCCESS;
 }
 
+vector<int> lessTau;
+
+
+bool cmp(const EDJoinResult& a,const EDJoinResult& b)
+{
+	if(a.id1 == b.id1)
+		return a.id2 < b.id2;
+	return a.id1 < b.id1;
+}
+
+bool operator==(const EDJoinResult& a,const EDJoinResult& b)
+{
+	return a.id1 == b.id1 && a.id2 == b.id2;
+}
+
 int SimJoiner::joinED(const char *filename1, const char *filename2, unsigned threshold, vector<EDJoinResult> &result) {
     result.clear();
-    readin(filename2);
+    tau = threshold;
+    readin(filename1,filename2);
     int index = 0;
-    int num = threshold * qq + 1;
+    unsigned id;
+
+    index = 0;
     for(vector<string>::iterator i = line_vec.begin();i != line_vec.end();++i)
     {
-    	vector<string> split_result;
-    	split_gram(*i,qq,split_result,MAX_NUM);
-    	//int overlap_th = i->length() + 1 - qq - threshold * qq;
-    	for(vector<string>::iterator j = split_result.begin();j != split_result.end();++j)
+    	if(i->length() <= tau)
     	{
-    		map<string,int>::iterator find_iter = gram_set.find(*j);
-    		if(find_iter == gram_set.end()) //not find
+    		//assert(false);
+    	}
+    	vector<string> split_result;
+    	split_partition(*i,i->length(),split_result,0);
+    	//assert(split_result.size() == tau + 1);
+    	Group& lengthGr = partition_inverted_list[i->length()];
+    	lengthGr.used = true;
+    	for(unsigned j = 0;j < split_result.size();j++)
+    	{
+    		string& substr = split_result[j];
+    		Segment& seg = lengthGr.segs[j];
+    		map<string,int>::iterator iter = seg.invMap.find(substr);
+    		if(iter == seg.invMap.end()) //not find
     		{
-    			gram_set[*j] = gram_inverted_list.size();
-    			gram_inverted_list.push_back(vector<int>());
+    			seg.invMap[substr] = seg.invList.size();
+    			seg.invList.push_back(vector<int>());
     		}
-    		gram_inverted_list[gram_set[*j]].push_back(index);
+    		seg.invList[seg.invMap[substr]].push_back(index);
     	}
     	index++;
     }
-    //cout << "build invert end\n";
-    //set<pair<unsigned,unsigned>> candidate;
-    ifstream fin(filename1);
-	unsigned id = 0;
-	string string_buf;
-	while(!fin.eof())
+
+	id = 0;
+	while(id < line_vec2.size())
 	{
-		getline(fin,string_buf);
-		if(string_buf.size() == 0)
+		string string_buf = line_vec2[id];
+		if(string_buf.length() == 0)
 		{
 			break;
 		}
-		int overlap_th = string_buf.size() + 1 - qq - threshold * qq;
-		//cout << "overlap:" << overlap_th << endl;
-		if(overlap_th > 0)
+		
+		set<int> candidate;
+		if(string_buf.length() <= tau)
 		{
-			vector<string> split_result;
-			split_gram(string_buf,qq,split_result,MAX_NUM);
-			map<unsigned,int> candidate;
-			//for(vector<string>::iterator j = split_result.begin();j != split_result.end();++j)
-			for(unsigned j = 0;j < split_result.size();++j)
-	    	{
-	    		//map<string,int>::iterator find_iter = gram_set.find(*j);
-	    		map<string,int>::iterator find_iter = gram_set.find(split_result[j]);
-	    		if(find_iter != gram_set.end()) // find
-	    		{
-	    			vector<int>& tmp_list = gram_inverted_list[gram_set[split_result[j]]];
-	    			for(vector<int>::iterator k = tmp_list.begin();k != tmp_list.end();++k)
-	    			{
-	    				//candidate.insert(make_pair(id,(unsigned)*k));
-	    				candidate[*k]++;
-	    				//cout << "pair : " << id << " " << *k << endl;
-	    			}
-	    		}
-	    	}
-	    	for(map<unsigned,int>::iterator it = candidate.begin();it != candidate.end();++it)
-	    	{
-	    		//cout << it->first << "|" << it->second << endl;
-	    		if(it->second < overlap_th)
-	    			continue;
-	    		unsigned ed = (unsigned)calED(string_buf,line_vec[it->first],threshold);
-	    		if(ed <= threshold)
-	    		{
-	    			EDJoinResult r;
-	    			r.id1 = (unsigned)id;
-	    			r.id2 = it->first;
-	    			r.s = ed;
-	    			//cout << r.id1 << " & " << r.id2 << " : " << r.s << endl;
-	    			result.push_back(r);
-	    		}
-	    	}
-    	}
-    	else
-    	{
-    		int filename1_id = 0;
-    		for(vector<string>::iterator it = line_vec.begin();it != line_vec.end();++it)
-	    	{
-	    		unsigned ed = (unsigned)calED(string_buf,*it,threshold);
-	    		if(ed <= threshold)
-	    		{
-	    			EDJoinResult r;
-	    			r.id1 = (unsigned)id;
-	    			r.id2 = filename1_id;
-	    			r.s = ed;
-	    			//cout << r.id1 << " & " << r.id2 << " : " << r.s << endl;
-	    			result.push_back(r);
-	    		}
-	    		filename1_id++;
-	    	}
-    	}
+			//assert(false);
+		}
+		unsigned lo = string_buf.length() - tau;
+		unsigned hi = string_buf.length();
+		for(unsigned len = lo;len <= hi;len++)
+		{
+			Group& lengthGr = partition_inverted_list[len];
+			if(lengthGr.used == false)
+				continue;
+			for(unsigned offset = 0;offset <= string_buf.length() - len;offset++)
+			{
+				vector<string> split_result;
+				split_partition(string_buf,len,split_result,offset);
+				//assert(split_result.size() == tau + 1);
+				for(unsigned index2 = 0;index2 < tau + 1;index2++)
+				{
+					Segment& segs = lengthGr.segs[index2];
+					map<string,int>& invMap = segs.invMap;
+					map<string,int>::iterator iter = invMap.find(split_result[index2]);
+					if(iter != invMap.end())
+					{
+						unsigned a = invMap[split_result[index2]];
+						vector<int>& vec = segs.invList[a];
+						for(vector<int>::iterator it = vec.begin();it != vec.end();it++)
+						{
+							//assert(line_vec[*it].length() <= hi);
+							candidate.insert(*it);
+						}
+					}
+				}
+			}
+		}
+
+		for(set<int>::iterator it = candidate.begin();it != candidate.end();++it)
+		{
+			unsigned ed = (unsigned)calED(string_buf,line_vec[*it],tau);
+			if(ed <= tau)
+			{
+				EDJoinResult r;
+				r.id2 = (unsigned)id;
+				r.id1 = (unsigned)*it;
+				r.s = ed;
+				//cout << r.id1 << " & " << r.id2 << " : " << r.s << endl;
+				result.push_back(r);
+			}
+		}
 		id++;
 	}
-	fin.close();
+
+	//-------------------------------------
+
+	index = 0;
+    for(vector<string>::iterator i = line_vec2.begin();i != line_vec2.end();++i)
+    {
+    	if(i->length() <= tau)
+    	{
+    		//assert(false);
+    	}
+    	vector<string> split_result;
+    	split_partition(*i,i->length(),split_result,0);
+    	//assert(split_result.size() == tau + 1);
+    	Group& lengthGr = partition_inverted_list2[i->length()];
+    	lengthGr.used = true;
+    	for(unsigned j = 0;j < split_result.size();j++)
+    	{
+    		string& substr = split_result[j];
+    		Segment& seg = lengthGr.segs[j];
+    		map<string,int>::iterator iter = seg.invMap.find(substr);
+    		if(iter == seg.invMap.end()) //not find
+    		{
+    			seg.invMap[substr] = seg.invList.size();
+    			seg.invList.push_back(vector<int>());
+    		}
+    		seg.invList[seg.invMap[substr]].push_back(index);
+    	}
+    	index++;
+    }
+
+	id = 0;
+	while(id < line_vec.size())
+	{
+		string string_buf = line_vec[id];
+		if(string_buf.length() == 0)
+		{
+			break;
+		}
+		
+		set<int> candidate;
+		if(string_buf.length() <= tau)
+		{
+			//assert(false);
+		}
+		unsigned lo = string_buf.length() - tau;
+		unsigned hi = string_buf.length();
+		for(unsigned len = lo;len < hi;len++)
+		{
+			Group& lengthGr = partition_inverted_list2[len];
+			if(lengthGr.used == false)
+				continue;
+			for(unsigned offset = 0;offset <= string_buf.length() - len;offset++)
+			{
+				vector<string> split_result;
+				split_partition(string_buf,len,split_result,offset);
+				//assert(split_result.size() == tau + 1);
+				for(unsigned index2 = 0;index2 < tau + 1;index2++)
+				{
+					Segment& segs = lengthGr.segs[index2];
+					map<string,int>& invMap = segs.invMap;
+					map<string,int>::iterator iter = invMap.find(split_result[index2]);
+					if(iter != invMap.end())
+					{
+						unsigned a = invMap[split_result[index2]];
+						vector<int>& vec = segs.invList[a];
+						for(vector<int>::iterator it = vec.begin();it != vec.end();it++)
+						{
+							candidate.insert(*it);
+						}
+					}
+				}
+			}
+		}
+
+		for(set<int>::iterator it = candidate.begin();it != candidate.end();++it)
+		{
+			unsigned ed = (unsigned)calED(string_buf,line_vec2[*it],tau);
+			if(ed <= tau)
+			{
+				EDJoinResult r;
+				r.id1 = (unsigned)id;
+				r.id2 = (unsigned)*it;
+				r.s = ed;
+				//cout << r.id1 << " & " << r.id2 << " : " << r.s << endl;
+				result.push_back(r);
+			}
+		}
+		id++;
+	}
+
+	sort(result.begin(), result.end(), cmp);
+	result.erase(unique(result.begin(),result.end()),result.end());
 
     return SUCCESS;
 }
@@ -249,6 +385,33 @@ void SimJoiner::printDebug()
 		cout << *it << "\n";
 	}
 	cout << "---------------" << endl;
+
+	for(int i = 0;i < 500;i++)
+	{
+		Group& lengthGr = partition_inverted_list[i];
+		if(lengthGr.used == false)
+			continue;
+		cout << "length : " << i <<  endl;
+		for(unsigned j = 0;j < tau + 1;j++)
+		{
+			Segment& segs = lengthGr.segs[j];
+			for(map<string,int>::iterator iter = segs.invMap.begin();iter != segs.invMap.end();iter++)
+			{
+				cout << iter->first << ": \n";
+				unsigned a = iter->second;
+				vector<int>& vec = segs.invList[a];
+				for(vector<int>::iterator it = vec.begin();it != vec.end();it++)
+				{
+					cout << *it << endl;
+				}
+			}
+		}
+
+	}
+
+
+	cout << "---------------" << endl;
+	/*
 	for(map<string,int>::iterator it = gram_set.begin();it != gram_set.end();++it)
 	{
 		int index = it->second;
@@ -260,5 +423,6 @@ void SimJoiner::printDebug()
 		}
 		cout << endl;
 	}
+	*/
 	cout << "end-------------------------\n";
 }
